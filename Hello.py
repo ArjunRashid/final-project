7c513ba91771adb89b78ee792ec7cbbe9384184f
Arjun Rashid Dimanarig <118730440+ArjunRashid@users.noreply.github.com>
2024-05-14 06:23:17 +0000

    .

--- Hello.py
+++ Hello.py
@@ -14,6 +14,7 @@
 
 import streamlit as st
 from streamlit.logger import get_logger
+from PIL import Image
 
 LOGGER = get_logger(__name__)
 
@@ -24,27 +25,21 @@ def run():
         page_icon="👋",
     )
 
-    st.write("# Welcome to Streamlit! 👋")
+    st.title("GROUP 11")
 
-    st.sidebar.success("Select a demo above.")
 
-    st.markdown(
-        """
-        Streamlit is an open-source app framework built specifically for
-        Machine Learning and Data Science projects.
-        **👈 Select a demo from the sidebar** to see some examples
-        of what Streamlit can do!
-        ### Want to learn more?
-        - Check out [streamlit.io](https://streamlit.io)
-        - Jump into our [documentation](https://docs.streamlit.io)
-        - Ask a question in our [community
-          forums](https://discuss.streamlit.io)
-        ### See more complex demos
-        - Use a neural net to [analyze the Udacity Self-driving Car Image
-          Dataset](https://github.com/streamlit/demo-self-driving)
-        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
-    """
-    )
+    st.write("Members:")
+    st.write("1. Brigola, Randolf")
+    st.write("2. Dimanarig, Arjun Rashid L.")
+    st.write("3. Periabras, Ellyza")
+
+
+    st.markdown("---")
+
+    st.write("Introduction")
+
+    st.write("  This project is focused on creating an application that implements different cryptographic techniques to ensure secure communication, data, and information exchange. The goal is to make the application user-friendly and accessible. Cryptography is essential for maintaining the security and trustworthiness of messages and data in today's world. This application offers a user-friendly interface for encrypting, decrypting, and hashing messages or files using various cryptographic algorithms.")
+
 
 
 if __name__ == "__main__":
